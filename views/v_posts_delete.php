83e0906cb8f497a93cf467a947610ad0ba0d0188
joseph buldini <jbul707@gmail.com>
2013-11-05 14:01:03 -0500

    Update v_posts_delete.php

--- views/v_posts_delete.php
+++ views/v_posts_delete.php
@@ -1,6 +1,5 @@
 
-
-<form action="/posts/delete/<?=$posts['post_id']?>" method="posts">
+<form action="/posts_delete/<?=$posts['post_id']?>" method="posts">
 	Are you sure you want to delete?
 	<input type="submit" name="delete" value="delete"/>
 </form>
