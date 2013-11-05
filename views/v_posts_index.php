ce8e2147d038158fee8ab2dcd913965f97555807
joseph buldini <jbul707@gmail.com>
2013-11-05 15:14:10 -0500

    Update v_posts_index.php

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -16,7 +16,7 @@
 </article>
 <br>
 
-	<form action="/posts/p_delete/">
+	<form action="/posts/posts_delete/">
 	<input type="hidden" name="post_id" value="<?$post['post_id']?>">
 	<input type="submit" value="delete">
 
