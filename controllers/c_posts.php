3ae75d4f9e737adb80f8edcd9ade6a3b8f912ecf
joseph buldini <jbul707@gmail.com>
2013-11-05 14:15:04 -0500

    Update c_posts.php

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -76,7 +76,7 @@ class posts_controller extends base_controller {
 
     }
 
-    public function posts_delete()  {
+    public function p_delete()  {
 
     # Setup view
     $this->template->content = View::instance('v_posts_delete');
