72b41efe31701789cecdf3a19514a5a2069580bd
joseph buldini <jbul707@gmail.com>
2013-11-06 07:18:35 -0500

    Update v_posts_delete.php

--- views/v_posts_delete.php
+++ views/v_posts_delete.php
@@ -1,6 +1,6 @@
 <h3>Are you sure you want to delete this post?</h3>
 <br>
-<a href="/posts/p_delete/<?=$post_id?>"Yes</a>
+<a href="/posts/p_delete/<?=$post_id?>">Yes</a>
 <a href="/posts/">No</a>
 
 
