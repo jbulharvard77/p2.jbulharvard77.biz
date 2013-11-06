a4f624348a39b7eec8737248575dc6b1caf7a898
joseph buldini <jbul707@gmail.com>
2013-11-06 11:44:01 -0500

    Update v_users_update.php

--- views/v_users_update.php
+++ views/v_users_update.php
@@ -3,11 +3,11 @@
 <form method='POST' action='/users/p_update/<?=$user_id?>'>
         
         First Name: <input type='text' name='first_name'>
-                value='<?php if(isset($_POST['first_name'])) echo $_POST['first_name']?>'><br>
+                value='<?php if(isset($_POST['first_name']))?>'><br>
         Last Name:  <input type='text' name='last_name'>
-                value='<?php if(isset($_POST['last_name'])) echo $_POST['last_name']?>'><br>
+                value='<?php if(isset($_POST['last_name']))?>'><br>
         Email:      <input type='text' name='email'>
-                value='<?php if(isset($_POST['email'])) echo $_POST['email']?>'><br>
+                value='<?php if(isset($_POST['email']))?>'><br>
 
         <input type='submit' value='Update'>
 
