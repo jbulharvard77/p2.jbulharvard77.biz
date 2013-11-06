316122d884835bd2ec456fb629462c8c25b5eeec
joseph buldini <jbul707@gmail.com>
2013-11-06 10:54:05 -0500

    Update v_users_update.php

--- views/v_users_update.php
+++ views/v_users_update.php
@@ -1,6 +1,6 @@
 <h2>Update Profile</h2>
 <div id = "olduser2">
-<form method='POST' action='/users/p_update'>
+<form method='POST' action='/users/p_update/<?=$user_id['user_id'?>'>
         
         First Name: <input type='text' name='first_name'><br>
         Last Name:  <input type='text' name='last_name'><br>
