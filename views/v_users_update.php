8a111e3de40f6754ce9621e3ee2786837fc0bc19
joseph buldini <jbul707@gmail.com>
2013-11-06 08:25:23 -0500

    Create v_users_update.php

---
+++ views/v_users_update.php
@@ -0,0 +1,12 @@
+<h2>Update Profile</h2>
+<div id = "olduser2">
+<form method='POST' action='/users/p_signup'>
+        
+        First Name: <input type='text' name='first_name'><br>
+        Last Name:  <input type='text' name='last_name'><br>
+        Email:      <input type='text' name='email'><br>
+
+        <input type='submit' value='Update'>
+
+</form>
+</div>
