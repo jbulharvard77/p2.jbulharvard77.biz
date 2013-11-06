11f3d886f26d7f7f0958f9b503c476508da5c580
joseph buldini <jbul707@gmail.com>
2013-11-06 13:54:25 -0500

    Update c_users.php

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -25,6 +25,8 @@ class users_controller extends base_controller {
         $_POST['token'] = sha1(TOKEN_SALT.$_POST['email'].Utils::generate_random_string());
 
         DB::instance(DB_NAME)->insert_row('users', $_POST);
+        
+        Router::redirect('/user/profile/');
 
     }
     
