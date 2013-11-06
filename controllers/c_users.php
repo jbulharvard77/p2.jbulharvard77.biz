93692d1451aa336f77906770321d3eb9cfcd91dd
joseph buldini <jbul707@gmail.com>
2013-11-06 14:14:23 -0500

    Update c_users.php

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -26,7 +26,7 @@ class users_controller extends base_controller {
 
         DB::instance(DB_NAME)->insert_row('users', $_POST);
         
-        Router::redirect('/users/profile/');
+        Router::redirect('/users/login/');
 
     }
     
