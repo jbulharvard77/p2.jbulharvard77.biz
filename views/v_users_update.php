45b6acf47b72bd4c39804ff0c2684a1994a30f8e
joseph buldini <jbul707@gmail.com>
2013-11-06 11:07:57 -0500

    Update v_users_update.php

--- views/v_users_update.php
+++ views/v_users_update.php
@@ -2,9 +2,12 @@
 <div id = "olduser2">
 <form method='POST' action='/users/p_update/<?=$user_id['user_id']?>'>
         
-        First Name: <input type='text' name='first_name'><br>
-        Last Name:  <input type='text' name='last_name'><br>
-        Email:      <input type='text' name='email'><br>
+        First Name: <input type='text' name='first_name'>
+                value='<?php if(isset($_POST['first_name'])) echo $_POST['first_name']?>'><br>
+        Last Name:  <input type='text' name='last_name'>
+                value='<?php if(isset($_POST['last_name'])) echo $_POST['last_name']?>'><br>
+        Email:      <input type='text' name='email'>
+                value='<?php if(isset($_POST['email'])) echo $_POST['email']?>'><br>
 
         <input type='submit' value='Update'>
 
